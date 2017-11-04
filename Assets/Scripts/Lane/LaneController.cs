b94d588065d1c37abc55dddb5d2c8720c15d4f75
Christoffer - from home <chris.krakou@gmail.com>
2017-11-04 17:36:57 +0100

    The lane now moves and can be spawned

---
+++ Assets/Scripts/Lane/LaneController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LaneController : MonoBehaviour
+{
+
+    public bool Debugging;
+    public GameObject LeftTile;
+    public GameObject RightTile;
+    public GameObject BlockerPrefab;
+    public GameObject FencePrefab;
+    public Transform[] ObstacleRows;
+
+    private List<Transform> obstacleSpots;
+
+    // Use this for initialization
+    void Start()
+    {
+        obstacleSpots = new List<Transform>();
+        foreach (var row in ObstacleRows)
+        {
+            foreach (var spot in row.GetComponentsInChildren<Transform>())
+            {
+                if (ObstacleRows.Contains(spot) == false)
+                {
+                    obstacleSpots.Add(spot);
+                }
+            }
+        }
+        if (Debugging)
+            Debug.Log("LaneController: Added " + obstacleSpots.Count);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SectionController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SectionController : MonoBehaviour {
+
+    public bool Debugging;
+
+    private void Awake()
+    {
+        if (Debugging)
+        {
+            foreach (var laneController in GetComponentsInChildren<LaneController>())
+            {
+                laneController.Debugging = true;
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
