3138d9d7eb1784b1ca132821d2cb1b630fdf5995
Christoffer Krakou <christoffer@krakou.dk>
2017-11-08 14:33:37 +0100

    Added skeleton files for score management and difficulty

---
+++ Assets/Scripts/GameManager/DifficultyManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyManager : MonoBehaviour {
+
+    public DifficultyLevel[] DifficultyLevels;
+    public float PointsThreshold;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+}
+
+[System.Serializable]
+public struct DifficultyLevel
+{
+    public float DistanceBetweenSpawns;
+    public float Speed;
+}
+
---
+++ Assets/Scripts/GameManager/ScoreKeeper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour {
+
+    public int Score;
+    public GameObject GUI;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
