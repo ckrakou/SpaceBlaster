d52980e99bc4deb9d516fc3c5fe1c9bc0a76e225
Christoffer - from home <chris.krakou@gmail.com>
2017-11-18 19:32:49 +0100

    Game now ends and restarts as expected

--- Assets/Scripts/GameManager/GameFlowManager.cs
+++ Assets/Scripts/GameManager/GameFlowManager.cs
@@ -2,28 +2,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameFlowManager : MonoBehaviour
 {
+    public bool Debugging;
+
+    private bool hasEnded;
+    private Scene currentScene;
+
     // Use this for initialization
     void Start()
     {
 
+        currentScene = SceneManager.GetActiveScene();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (hasEnded)
+        {
+            if (Input.GetAxis("Shoot") > 0)
+            {
+                if (Debugging)
+                {
+                    Debug.Log("GameFlowManager: Restarting");
+                }
+                SceneManager.LoadScene(currentScene.buildIndex);
+            }
+        }
     }
 
     public void EndGame()
     {
+        if (Debugging)
+        {
+            Debug.Log("GameFlowManager: Ending Game");
+        }
         foreach (var obstacle in GameObject.FindGameObjectsWithTag("Section"))
         {
             obstacle.GetComponent<TileRunner>().Speed = 0;
         }
         this.gameObject.GetComponent<TileSpawner>().enabled = false;
         GameObject.Find("Player").GetComponent<PlayerControl>().enabled = false;
+        hasEnded = true;
     }
 }
