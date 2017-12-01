7135738e4cd5fce91e3dec980988d2c67f0584ce
Christoffer - from home <chris.krakou@gmail.com>
2017-12-01 22:02:19 +0100

    Added oscilating light, for attentions sake

--- Assets/Scripts/Juice/SplashScreenJuice.cs
+++ Assets/Scripts/Juice/SplashScreenJuice.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using System;
 
 public class SplashScreenJuice : MonoBehaviour {
 
     public float FadeTime = 1.0f;
+    [Range(1,100)]
+    public int DarkenedPercentage = 75;
 
     private Image splashScreen;
+    private bool oscilating = false;
 	// Use this for initialization
 	void Start () {
         splashScreen = GetComponent<Image>();
@@ -18,13 +22,26 @@ public class SplashScreenJuice : MonoBehaviour {
     public void FadeIn()
     {
         splashScreen.DOKill();
-        splashScreen.DOFade(1, FadeTime);
+        splashScreen.DOFade(1, FadeTime).onComplete = new TweenCallback(StartOscilating);
+
     }
 
     public void FadeOut()
     {
         splashScreen.DOKill();
-        splashScreen.DOFade(0, FadeTime);
+        splashScreen.DOFade(0, FadeTime).onComplete = new TweenCallback(StopOscilating);
     }
 
+    private void StartOscilating()
+    {
+        Sequence colorShift = DOTween.Sequence();
+        colorShift.Append(splashScreen.DOColor(new Color(DarkenedPercentage / 100f, DarkenedPercentage / 100f, DarkenedPercentage / 100f), 0.5f));
+        colorShift.Append(splashScreen.DOColor(Color.white, 0.5f));
+        colorShift.SetLoops(-1, LoopType.Yoyo);
+    }
+
+    private void StopOscilating()
+    {
+        splashScreen.color = Color.white;
+    }
 }
